1517ea07bc2096f3d70d7be5032060c6f9ae1eba
poompongphun <poompongphun@gmail.com>
2023-10-19 22:55:42 +0700

    80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set("view engine", "ejs");
 // app.use(express.urlencoded({ extended: true }));
